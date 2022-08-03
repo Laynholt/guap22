b66873ed305b8c1f5307f66083b2eac194da4ff6
Mike <41357381+Laynholt@users.noreply.github.com>
2022-08-03 23:00:18 +0300

    Add files via upload

--- guap22.py
+++ guap22.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 
 
-def menu(data):
+def menu(data, date):
     """
     Функция для выбора и сортировки по пределенным параметрам
     :param data: Датафрейм pandas
@@ -17,7 +19,7 @@ def menu(data):
     #     data.to_excel(filename, sheet_name='09.04.00', index=False, encoding='utf-8-sig')
     #     print(f"Файл сохранен как {filename}")
 
-    filename = 'guap22.xlsx'
+    filename = f'guap-{date}.xlsx'
     with pd.ExcelWriter(filename) as writer:
         data.to_excel(writer, sheet_name='09.04.00', index=False, encoding='utf-8-sig')
         print(f"Файл сохранен как {filename}\n")
@@ -28,7 +30,7 @@ def menu(data):
             data_headers = data.columns
 
             while not end:
-                sheet_name = 'sorted_by'
+                sheet_name = 'Фильтр по'
                 sorted_by = ''
                 was_sorted = False
                 didnt_choose_anything = 'Доступные параметра фильтровки закончились! :(\n'
@@ -38,9 +40,9 @@ def menu(data):
                 if choice.lower() == 'да':
                     didnt_choose_anything = ''
                     was_sorted = True
-                    sheet_name += '_bal'
 
                     number_of_points = int(input('Введите от скольки баллов начинать (включительно):\n>> '))
+                    sheet_name += f' баллам{number_of_points}'
                     sorted_by += f'имеют =>{number_of_points} баллов'
                     clipped_data = clipped_data[clipped_data[data_headers[4]] >= number_of_points]
 
@@ -48,7 +50,7 @@ def menu(data):
                 if choice.lower() == 'да':
                     didnt_choose_anything = ''
                     was_sorted = True
-                    sheet_name += '_sogl'
+                    sheet_name += ' согл'
 
                     if len(sorted_by) != 0:
                         sorted_by += ', подали согласие на зачисление'
@@ -61,7 +63,7 @@ def menu(data):
                 if choice.lower() == 'да':
                     didnt_choose_anything = ''
                     was_sorted = True
-                    sheet_name += '_doc'
+                    sheet_name += ' докам'
 
                     if len(sorted_by) != 0:
                         sorted_by += ', подали оригиналы документов'
@@ -87,20 +89,30 @@ def get_table_data(url: str):
     """
     Функция считывает данные с сайта и возвращает датафрейм
     :param url: URL сайта
-    :return: Pandas dataframe
+    :return: (Pandas dataframe, date: str)
     """
-    table_data = pd.read_html(url)
-    table_data = table_data[0]
+
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'lxml')
+
+    table = soup.find_all('table')
+    table_data = pd.read_html(str(table))[0]
     data_headers = table_data.columns
     col_edited = data_headers[1]
     table_data[col_edited].fillna('Нет', inplace=True)
 
-    return table_data
+    date = ''
+    for category in soup('b', text=lambda text: text and text == 'Дата актуализации - '):
+        date = category.next_sibling.strip('" \n')
+    print(f'Данные актуальны на {date}')
+
+    return table_data, date.replace(':', '-')
 
 
 def main():
     url = 'https://priem.guap.ru/_lists/List_1725_14'
-    menu(get_table_data(url))
+    table, date = get_table_data(url)
+    menu(table, date)
 
 
 if __name__ == '__main__':
