691a6a0329455263229b969b9de576c50d04efb5
Mike <ne.free532@gmail.com>
2022-08-06 18:41:57 +0300

    фикс

--- guap22-ext-ui.py
+++ guap22-ext-ui.py
@@ -12,7 +12,7 @@ class AppParser:
         # ui variables
         self.window = tkinter.Tk()
         self.window.geometry('560x100')
-                try:
+        try:
             self.window.iconbitmap('icon.ico')
         except tkinter.TclError:
             pass
